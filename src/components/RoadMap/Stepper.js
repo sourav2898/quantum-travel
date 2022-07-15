0cbad816bc1ababe26a36aaf77c435b0cf936ddc
Sourav Kumar <souravkumardubey2898@gmail.com>
2022-07-15 22:26:20 +0530

    feat/road-map stepper

---
+++ src/components/RoadMap/Stepper.js
@@ -0,0 +1,91 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const Stepper = ({ steps }) => {
+  return (
+    <>
+      {steps?.map((value, index) => {
+        return (
+          <Box
+            key={index}
+            sx={{
+              display: "flex",
+              alignItems: "flex-start",
+              justifyContent: "center",
+              width: "80%",
+              height: "100%",
+            }}
+          >
+            <Typography
+              sx={{
+                display: { md: "block", sm: "none", xs: "none" },
+                width: "20%",
+                textAlign: "right",
+                marginRight: 2,
+              }}
+              variant="h6"
+            >
+              {" "}
+              {value?.label}
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                marginRight: 2,
+                height: "100%",
+              }}
+            >
+              <Box
+                sx={{
+                  width: 20,
+                  height: 20,
+                  borderRadius: "50%",
+                  border: "2px solid blue",
+                  backgroundColor: "black",
+                  marginBottom: 0,
+                  padding: 0,
+                }}
+              />
+              {index !== steps.length - 1 && (
+                <Box
+                  sx={{
+                    width: 2,
+                    backgroundColor: "blue",
+                    minHeight: { md: 300, sm: 280, xs: 250 },
+                  }}
+                />
+              )}
+            </Box>
+            <Box
+              sx={{
+                width: { md: 150, sm: 60, xs: 60 },
+                height: 2,
+                backgroundColor: "blue",
+                marginRight: 2,
+                marginTop: 1,
+              }}
+            />
+            <Box
+              sx={{ width: { md: "40%", sm: "100%", xs: "100%" }, margin: 0 }}
+            >
+              <Typography
+                sx={{
+                  display: { md: "none", sm: "block", xs: "block" },
+                }}
+                variant="h6"
+              >
+                {" "}
+                {value?.label}
+              </Typography>
+              <Typography> {value?.description}</Typography>
+            </Box>
+          </Box>
+        );
+      })}
+    </>
+  );
+};
+
+export default Stepper;
--- src/components/RoadMap/index.js
+++ src/components/RoadMap/index.js
@@ -1,38 +1,75 @@
-import {
-  Box,
-  Container,
-  Step,
-  StepContent,
-  StepLabel,
-  Stepper,
-  Typography,
-} from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import roadMapImg from "../../assets/img/roadmap.png";
+import Stepper from "./Stepper";
 
 const RoadMap = () => {
   const { t } = useTranslation(["roadmap"]);
-  const steps = [
-    {
-      label: "Select campaign settings",
-      description: `For each ad campaign that you create, you can control how much
+  const steps = {
+    englishSteps: [
+      {
+        label: "Select campaign settings",
+        description: `For each ad campaign that you create, you can control how much
               you're willing to spend on clicks and conversions, which networks
               and geographical locations you want your ads to show on, and more.`,
-    },
-    {
-      label: "Create an ad group",
-      description:
-        "An ad group contains one or more ads which target a shared set of keywords.",
-    },
-    {
-      label: "Create an ad",
-      description: `Try out different ad text to see what brings in the most customers,
+      },
+      {
+        label: "Create an ad group",
+        description:
+          "An ad group contains one or more ads which target a shared set of keywords.",
+      },
+      {
+        label: "Create an ad",
+        description: `Try out different ad text to see what brings in the most customers,
               and learn how to enhance your ads using features like ad extensions.
               If you run into any problems with your ads, find out how to tell if
               they're running and how to resolve approval issues.`,
-    },
-  ];
+      },
+    ],
+    spainSteps: [
+      {
+        label: "DISEÑO INICIAL",
+        description: `Diseño y conceptualización del proyecto QUANTUM pzfra el proyecto fisico. Q1 2021`,
+      },
+      {
+        label: "UBICACIÓN",
+        description: `Contacto con entidades interesadas en alojar el proyecto QUANTUM fisico. Q2 2021`,
+      },
+      {
+        label: "DISEÑO METAVERSO",
+        description: `Diseño y conceptualización del proyecto QUANTUM para el proyecto virtal. Q1 2022`,
+      },
+      {
+        label: "VIRTUALIZACIÓN",
+        description: `Selección y virtualización de piezas para NFT de juego, galería y y museo. Q1 2022`,
+      },
+      {
+        label: "CREACIÓN METAVERSO",
+        description: `Busqueda de socios y empresas apra la creación y promoción de QUANTUM METAVERSE. Q2 2022`,
+      },
+      {
+        label: "SUBASTA INICIAL",
+        description: `Creación de la primera subasta de piezas QUANTUM. Q3 2022`,
+      },
+      {
+        label: "INAUGURACIÓN",
+        description: `Inauguración galería de arte THE ART OF NATURE en el metaverso. .Q4 2023`,
+      },
+      {
+        label: "INAUGURACIÓN",
+        description: `Inauguración NFT GAME DINIPETREA en el metaverso. .Q1 2023`,
+      },
+      {
+        label: "INAUGURACIÓN",
+        description: `Inauguración Museo QUANTUM en el metaverso. .Q2 2023`,
+      },
+      {
+        label: "CONSTRUCCIÓN",
+        description: `Inicio construcción QUANTUM fisico. .Q3 2023`,
+      },
+    ],
+  };
   return (
     <Container
       id="roadmap"
@@ -55,6 +92,18 @@ const RoadMap = () => {
         {t("roadmap")}
       </Typography>
       <img width="100%" src={roadMapImg} alt="roadmap" />
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          marginTop: 5,
+        }}
+      >
+        <Stepper steps={steps[t("steps")]} />
+      </Box>
     </Container>
   );
 };
